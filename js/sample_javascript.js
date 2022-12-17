56e7cd87b379d54ca1bce9d318c0a40ebf70c4ea
yuyasurarin <yuyasurarin@gmail.com>
2022-12-17 15:33:35 +0900

    Change js

--- js/sample_javascript.js
+++ js/sample_javascript.js
@@ -1,8 +1,6 @@
-JavaScript
-
-const score_view = document.getElementById("score");
 
 function alertButton(){
+    const score_view = document.getElementById("score");
     a = a + 1;
     console.log(a);
     score_view.innerText = a;
