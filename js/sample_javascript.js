2b5b0691797b2522545933fa62982bb9944202a5
yuyasurarin <yuyasurarin@gmail.com>
2022-12-17 16:40:33 +0900

    Add click level

--- js/sample_javascript.js
+++ js/sample_javascript.js
@@ -5,10 +5,15 @@ function addscore(lv) {
     console.log(a);
     score_view.innerText = a;
 }
+function click_up(){
+    const click = document.getElementById("clicklevel");
+    click_level += 1;
+    click.innerText = "クリックレベルLv." + click_level;
+}
 function auto_click() {
     const autoclick = document.getElementById("autoclick");
-    let autoclick_lv = parseInt(autoclick.innerText.split(".")[1])
+    let autoclick_lv = parseInt(autoclick.innerText.split(".")[1]);
     autoclick_lv += 1;
-    autoclick.innerText = "オートクリックLv." + autoclick_lv
-    lv = lv + 1
+    autoclick.innerText = "オートクリックLv." + autoclick_lv;
+    ps = ps + 1;
 }
