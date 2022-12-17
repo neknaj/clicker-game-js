7812dd748017fb308b879db50e563f07226c5310
yuyasurarin <yuyasurarin@gmail.com>
2022-12-17 16:29:43 +0900

    Change index.html and js

--- js/sample_javascript.js
+++ js/sample_javascript.js
@@ -1,7 +1,14 @@
 
-function alertButton(){
+function addscore(lv) {
     const score_view = document.getElementById("score");
-    a = a + 1;
+    a = a + lv;
     console.log(a);
     score_view.innerText = a;
+}
+function auto_click() {
+    const autoclick = document.getElementById("autoclick");
+    let autoclick_lv = parseInt(autoclick.innerText.split(".")[1])
+    autoclick_lv += 1;
+    autoclick.innerText = "オートクリックLv." + autoclick_lv
+    lv = lv + 1
 }
